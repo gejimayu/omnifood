d952861fd1dc16925ad8975cad541d6f6cc3c3b3
Gianfranco <gianffh@gmail.com>
2019-06-20 04:47:27 +0700

    Add animations on few elemnts on viewport

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,6 +1,9 @@
-// NAV BAR ANIMATION
+//	NAV BAR ANIMATION
 const header = document.querySelector('header');
-const callback = function (entries, observer) {
+const navBarOptions = {
+	rootMargin: "-60px 0px", //height of secondary nav bar
+}
+const navBarObserver = new IntersectionObserver(function (entries, observer) {
 	entries.forEach(function(entry) {
 		const navBar = document.querySelector('.header-nav');
 		if (entry.isIntersecting)  {
@@ -9,9 +12,25 @@ const callback = function (entries, observer) {
 			navBar.classList.add('sticky-nav');
 		}
 	})
+}, navBarOptions);
+navBarObserver.observe(header);
+
+// ANIMATION ON VIEWPORT
+function animateOnViewport(elementQuery, animationClassName) {
+	const element = document.querySelector(elementQuery);
+	const options = { threshold: 1 };
+	const observer = new IntersectionObserver(function (entries, observer) {
+		entries.forEach(function (entry) {
+			if (entry.isIntersecting) {
+				element.classList.add(animationClassName);
+				observer.unobserve(element);
+			}
+		});
+	}, options);
+	observer.observe(element);
 }
-const options = {
-	rootMargin: "-60px 0px", //height of secondary nav bar
-}
-const observer = new IntersectionObserver(callback, options);
-observer.observe(header);
+
+animateOnViewport('.section--feature__features', 'animation-fade-in');
+animateOnViewport('.content__left img', 'animation-fade-in-up');
+animateOnViewport('.section--cities__content', 'animation-fade-in');
+animateOnViewport('.section--plans__content .plan:first-child', 'animation-pulse');
